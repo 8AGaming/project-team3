5025258cc71af57ceacbac2049fdfbbf4851f800
Meirasulin <asulinmeir64@gmail.com>
2023-11-15 22:39:08 +0200

    cart update meir

--- src/features/cart/component/ProductInCart.tsx
+++ src/features/cart/component/ProductInCart.tsx
@@ -3,47 +3,38 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Box, Fab, Grid } from "@mui/material";
-import AddIcon from "@mui/icons-material/Add";
-import RemoveIcon from "@mui/icons-material/Remove";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { useState } from "react";
-import { productInCart } from "../types/productInCart";
+import { Grid } from "@mui/material";
+import { useAppSelector } from "../../../store/hooks";
+import ProductCartButton from "./ProductCartButton";
+import { PropProductInCart } from "../types/productInCart";
 
-const ProductInCart = (productOnCart: productInCart) => {
-  const [quantityState, setQuantity] = useState(productOnCart.quantity);
+const ProductInCart = ({ productCart }: PropProductInCart) => {
+  const cart = useAppSelector((state) => state.cart.cart);
 
-  const handelQuantity = (type: string) => {
-    if (type === "-") {
-      if (quantityState > 1) {
-        setQuantity(quantityState - 1);
-      } else {
-        setQuantity(quantityState);
-      }
-    } else if (type === "+") {
-      setQuantity(quantityState + 1);
-    }
-  };
+  const productIndexInCart = cart.findIndex(
+    (p) => p.product.title === productCart.product.title
+  );
+
   return (
     <Card sx={{ width: 250, margin: 1 }}>
       <CardMedia
         sx={{ height: 140, maxWidth: 250 }}
-        image={productOnCart.product.images[0]}
-        title={productOnCart.product.title}
+        image={productCart.product.thumbnail}
+        title={productCart.product.title}
       />
       <CardContent>
         <Typography gutterBottom variant="h5" component="div">
-          {productOnCart.product.title}
+          {productCart.product.title}
         </Typography>
         <Grid>
-          {productOnCart.product.discountPercentage &&
-          productOnCart.product.discountPercentage > 0 ? (
+          {productCart.product.discountPercentage &&
+          productCart.product.discountPercentage > 0 ? (
             <>
               <Typography variant="body1" sx={{ display: "inline", margin: 1 }}>
                 {parseFloat(
                   (
-                    ((100 - productOnCart.product.discountPercentage) / 100) *
-                    productOnCart.product.price
+                    ((100 - productCart.product.discountPercentage) / 100) *
+                    productCart.product.price
                   ).toFixed(1)
                 )}
                 $
@@ -54,39 +45,23 @@ const ProductInCart = (productOnCart: productInCart) => {
                 color="text.secondary"
                 sx={{ textDecoration: "line-through", display: "inline" }}
               >
-                {productOnCart.product.price}$
+                {productCart.product.price}$
               </Typography>
             </>
           ) : (
             <Typography variant="body1" sx={{ display: "inline" }}>
-              {productOnCart.product.price}$
+              {productCart.product.price}$
             </Typography>
           )}
         </Grid>
         <Grid>
           <Typography variant="body2" color="text.secondary">
-            quantity {quantityState}
+            quantity {cart[productIndexInCart].quantity}
           </Typography>
         </Grid>
       </CardContent>
       <CardActions>
-        <Box sx={{ "& > :not(style)": { m: 1 } }}>
-          <Fab size="small" color="inherit" onClick={() => handelQuantity("+")}>
-            <AddIcon />
-          </Fab>
-          <Fab
-            size="small"
-            color="inherit"
-            disabled={quantityState === 1 ? true : false}
-            onClick={() => handelQuantity("-")}
-          >
-            <RemoveIcon />
-          </Fab>
-          <Fab size="small" color="inherit">
-            {/* להוסיף טיפול במחיקה של מוצר מהמערך של עגלת הקניות */}
-            <DeleteIcon />
-          </Fab>
-        </Box>
+        <ProductCartButton productCart={productCart} />
       </CardActions>
     </Card>
   );
