5539ac5cfb273077a3e0a467797ba097d6b8e1a8
8AGaming <ap770ai@gmail.com>
2023-11-15 14:14:15 +0200

    test

--- src/features/layout/HeaderLoggedIn/HeaderLogo.tsx
+++ src/features/layout/HeaderLoggedIn/HeaderLogo.tsx
@@ -2,6 +2,7 @@ import AdbIcon from "@mui/icons-material/Adb";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import { useNavigate } from "react-router-dom";
+import React from "react";
 
 const HeaderLogo = () => {
   const navigate = useNavigate();
